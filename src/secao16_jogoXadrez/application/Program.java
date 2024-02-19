9a898f11ecb0342d435bbc95430c3dc649ab7442
ThiagoMdO <thiago_catureba@hotmail.com>
2024-02-19 14:29:38 -0300

    primeira class: position

--- src/secao16_jogoXadrez/application/Program.java
+++ src/secao16_jogoXadrez/application/Program.java
@@ -1,7 +1,11 @@
 package secao16_jogoXadrez.application;
 
+import secao16_jogoXadrez.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
