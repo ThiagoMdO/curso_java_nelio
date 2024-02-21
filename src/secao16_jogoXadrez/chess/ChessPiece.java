415915d6e1838aa14afdd21d5a4ae97fd85e5bdc
ThiagoMdO <thiago_catureba@hotmail.com>
2024-02-21 16:41:25 -0300

    Piece move count

--- src/secao16_jogoXadrez/chess/ChessPiece.java
+++ src/secao16_jogoXadrez/chess/ChessPiece.java
@@ -8,21 +8,35 @@ public abstract class ChessPiece extends Piece {
 
     private Color color;
 
-    public ChessPiece(Board board, Color color){
+    private int moveCount;
+
+    public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
     }
 
-    public ChessPosition getChessPosition(){
+    public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
 
-    protected boolean isThereOpponentPiece(Position position){
+    protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
     }
 }
